1e8587464e61339062fd050de64adbe119775b94
nikokarkkainen <niko.karkkainen@protacon.com>
2016-12-16 02:26:44 +0200

    Alavalikko ja refaktorointia sinne sun tänne.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { routing } from './app.routes';
 import { ProductComponent } from './product/product.component';
 import { HeaderComponent } from './header/header.component';
 import {ProductResolver} from "./product/resolves/product.resolver";
+import {FootermenuComponent} from "./footermenu/footermenu.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     HeaderComponent,
+    FootermenuComponent,
   ],
   imports: [
     BrowserModule,
